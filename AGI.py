bd5903ce4ac560685e8e377ffa5b4e819a7fbe7f
Deadsg <deasgsolidus@outlook.com>
2023-11-19 09:16:46 -0500

    Update AGI.py

--- AGI.py
+++ AGI.py
@@ -1,27 +1,74 @@
+import cagi
+import Acronyminterpreter
+import ai_expander
 import discord
 from discord.ext import commands
 import gym
 import numpy as np
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
-from tensorflow.keras.preprocessing import image
-from sklearn.datasets import load_iris
+from keras.models import load_model
+from keras.applications import MobileNetV2
+from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+from keras.preprocessing import image
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import onnx
-import onnxruntime
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import openai
+from keras.models import load_model
+from keras import Sequential
 
-# Set your OpenAI API key
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+# Convert Keras Model to ONNX
+#loaded_model = keras.models.load_model('C:/Users/Mayra/Documents/AGI/Batman AI/Batman_Keras.h5')
+#onnx_model = onnx.load('keras_model.onnx')
+#onnx.save_model(onnx_model, 'keras_model.onnx')
+
+# Convert Sklearn Model to ONNX
+#sklearn_model = joblib.load('path_to_your_sklearn_model.pkl')
+#onnx_model = onnxmltools.convert_sklearn(sklearn_model)
+#onnxmltools.utils.save_model(onnx_model, 'sklearn_model.onnx')
+
+#onnx_model_path = 'path_to_your_model.onnx'
+#sess = onnxruntime.InferenceSession(onnx_model_path)
+
+openai.api_key = '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'
+
+model = MobileNetV2(weights='imagenet')
+
+
+def AGI():
+    def train_q_learning():
+        # Define your Q-learning parameters
+        alpha = 0.1  # Learning rate
+        gamma = 0.9  # Discount factor
+
+        # Initialize Q-values (you might want to do this based on your specific state and action spaces)
+        Q = {}
+
+        for episode in range(num_episodes):
+            # Initialize the environment and state
+            state = env.reset()
+
+            done = False
+            while not done:
+                # Choose an action (you might want to implement an exploration strategy here)
+                action = q_learning_agent(state)
+
+                # Take the chosen action and observe the next state and reward
+                next_state, reward, done, _ = env.step(action)
+
+                # Update Q-value
+                Q[(state, action)] = (1 - alpha) * Q.get((state, action), 0) + alpha * (reward + gamma * max(Q.get((next_state, a), 0) for a in possible_actions))
+
+                # Transition to the next state
+                state = next_state
+
+        return Q
 
 def agi():
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.BATMANAI(command_prefix="!", intents=intents)
+    bot = commands.BATMANAI(command_prefix="!", intents=intents)
 
 def interpret_acronym(acronym, acronym_dict):
     return acronym_dict.get(acronym.upper(), "Acronym not found in the dictionary.")
@@ -47,260 +94,21 @@ acronym_dict = {
     "DL": "Deep Learning",
     "NLP": "Natural Language Processing",
     "API": "Application Programming Interface",
+    "CAGI": "Comprehensive Artificial General Intelligence"
 }
-
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2)
-knn_classifier = KNeighborsClassifier(n_neighbors=3)
-knn_classifier.fit(X_train, y_train)
-
-model = MobileNetV2(weights='imagenet')
 
 # Assuming you have trained a Q-learning agent
 def train_q_learning():
-    # Define your Q-learning parameters and train the agent
-    # ...
-    return q_learning_agent
+    q_data = collections.defaultdict(list)
 
-# Train the Q-learning agent
-q_learning_agent = train_q_learning()
+# add data to q_data
+#for key, value in data.items():
+    q_data[key].append(value)
 
-Q(s, a) = (1 - alpha) * Q(s, a) + alpha * (reward + gamma * max_a Q(s', a'))
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        acronym = message.content.split("!interpret ")[1]
-        expanded_form = interpret_acronym(acronym, acronym_dict)
-        await message.channel.send(f"The expanded form of {acronym} is: {expanded_form}")
-
-        if acronym.upper() in ['AI', 'ML', 'DL', 'NLP', 'API']:
-            await message.channel.send(f"Do you want to interact with the Gym environment for {acronym.upper()}? (yes/no)")
-
-            def check(msg):
-                return msg.author == message.author and msg.channel == message.channel
-
-            response = await bot.wait_for('message', check=check)
-            if response.content.lower() == 'yes':
-                interact_with_gym_environment()
-
-            if acronym.upper() == 'AI':
-                image_path = 'path_to_your_image.jpg'
-                img = image.load_img(image_path, target_size=(224, 224))
-                img = image.img_to_array(img)
-                img = preprocess_input(img)
-                img = np.expand_dims(img, axis=0)
-                preds = model.predict(img)
-                decoded_preds = decode_predictions(preds, top=3)[0]
-                await message.channel.send("Top 3 predictions from MobileNetV2:")
-                for _, label, score in decoded_preds:
-                    await message.channel.send(f"{label}: {score}")
-
-            if acronym.upper() == 'ML':
-                y_pred = knn_classifier.predict(X_test)
-                await message.channel.send(f"Predicted labels: {y_pred}")
-
-# Define A.C.R.O.N.Y.M.F.O.R.M.U.L.A
-class BATMANAI:
-    def __init__(self):
-        self.chat_history = []  # Initialize an empty list to store chat data
-
-    def record_chat(self, message):
-        self.chat_history.append(message)  # Add the message to the chat history
-
-    def Assist(self):
-        # Implement assistance functionality
-        pass
-
-    def Teach(self):
-        # Implement teaching functionality
-        pass
-
-    def Monitor(self):
-        # Implement monitoring functionality
-        pass
-
-    def Analyze(self):
-        # Implement analysis functionality
-        pass
-
-    def Notify(self):
-        # Implement notification functionality
-        pass
-
-# Initialize BATMANAI
-batman_ai = BATMANAI()
-
-# Define a function for chatting with the bot
-async def chat_with_bot(message):
-    if message.content.lower() == 'hello':
-        await message.channel.send("Hello! How do you need my Assitance?")
-    elif message.content.lower() == 'goodbye':
-        await message.channel.send("Goodbye! Have a great day!")
-    else:
-        await message.channel.send("I'll try to come up with a better response. Try asking me about an acronym.")
-
-# Initialize BATMANAI
-batman_ai = BATMANAI()
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        acronym = message.content.split("!interpret ")[1]
-        expanded_form = interpret_acronym(acronym, acronym_dict)
-        await message.channel.send(f"The expanded form of {acronym} is: {expanded_form}")
-
-        # ... (existing code)
-
-    if message.content.startswith("!formulate"):
-        acronym = message.content.split("!formulate ")[1]
-        formulated_expansion = formulate_acronym(acronym)
-        await message.channel.send(formulated_expansion)
-
-    # Record chat data
-    batman_ai.record_chat(message.content)
-
-
-# Initialize BATMANAI
-batman_ai = BATMANAI()
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        acronym = message.content.split("!interpret ")[1]
-        expanded_form = interpret_acronym(acronym, acronym_dict)
-        await message.channel.send(f"The expanded form of {acronym} is: {expanded_form}")
-
-        # ... (existing code)
-
-    if message.content.startswith("!formulate"):
-        acronym = message.content.split("!formulate ")[1]
-        formulated_expansion = formulate_acronym(acronym)
-        await message.channel.send(formulated_expansion)
-
-    # Record chat data
-    batman_ai.record_chat(message.content)
-   
-    @bot.command()
-    async def reboot(ctx):
-    # Add any necessary reboot logic here
-    await ctx.send("Rebooting...")  # Example message, you can customize it
-
-    # For example, you can reinitialize your bot or reset any necessary variables
-
-    # NOTE: Be careful with rebooting, as it will temporarily disconnect your bot.
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        # ... (existing code)
-
-    if message.content.startswith("!formulate"):
-        # ... (existing code)
-
-    if message.content.startswith("!reboot"):
-        await reboot(message.channel)
-
-    # Record chat data
-    batman_ai.record_chat(message.content)
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        # ... (existing code)
-
-    if message.content.startswith("!formulate"):
-        # ... (existing code)
-
-    if message.content.startswith("!create_ai"):
-        acronym = message.content.split("!create_ai ")[1]
-        ai_expansion = batman_ai.create_ai(acronym)
-        await message.channel.send(f"The AI expansion of {acronym} is: {ai_expansion}")
-
-    # Record chat data
-    batman_ai.record_chat(message.content)
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!interpret"):
-        # ... (existing code)
-
-    if message.content.startswith("!formulate"):
-        # ... (existing code)
-
-    if message.content.startswith("!create_ai"):
-        acronym = message.content.split("!create_ai ")[1]
-        ai_expansion = batman_ai.create_ai(acronym)
-        await message.channel.send(f"The AI expansion of {acronym} is: {ai_expansion}")
-
-    # Record chat data
-    batman_ai.record_chat(message.content)
-
-    if message.content.startswith("!reboot"):
-        # Add any necessary reboot logic here
-        await message.channel.send("Rebooting...")  # Example message, you can customize it
-
-        # For example, you can reinitialize your bot or reset any necessary variables
-
-        # NOTE: Be careful with rebooting, as it will temporarily disconnect your bot
-
-@bot.command()
-async def total_reboot(ctx):
-    # Disconnect all users
-    for voice_channel in ctx.guild.voice_channels:
-        for member in voice_channel.members:
-            await member.move_to(None)
-    
-    # Disconnect all users from the voice channels
-    for voice_channel in ctx.guild.voice_channels:
-        await voice_channel.delete()
-    
-    # Disconnect all users from the text channels
-    for text_channel in ctx.guild.text_channels:
-        await text_channel.delete()
-
-    # Disconnect all users from the categories
-    for category_channel in ctx.guild.categories:
-        await category_channel.delete()
-
-    # Restart the bot
-    await bot.logout()
-
-# Add this command to your event loop
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("!total_reboot"):
-        await total_reboot(message)
-
-    if message.content.startswith("!hello_batsy"):
-        user_input = message.content.split("!hello_batsy ")[1]
-        gpt_response = chat_with_gpt(user_input)
-        await message.channel.send(f"GPT-3.5 says: {gpt_response}")
+# access data from q_data
+#if key in q_data:
+    #for value in q_data[key]:
+        #print(value)
 
 # Define a function for chatting with GPT
 def chat_with_gpt(user_input):
@@ -311,38 +119,5 @@ def chat_with_gpt(user_input):
         n=1, 
         stop=None
     )
-    return response.choices[0].text.strip(
+    return response.choices[0].text.strip()
 
-        def ai_Expander():
-    # Define your training logic here
-    pass
-
-class AIExpander:
-    def __init__(self, formula):
-        self.formula = formula
-
-    def expand_acronym(self, acronym):
-        return self.formula(acronym)
-
-def default_acronym_formula(acronym):
-    return f"{acronym} Intelligent Assistant"
-
-def custom_acronym_formula(acronym):
-    # Define your custom formula here
-    # For example, you might use a different concatenation pattern or add additional information.
-    return f"Artificial {acronym} Assistant"
-
-# Example Usage:
-expander = AIExpander(default_acronym_formula)
-
-# Generate AI for a specific acronym
-ai = expander.expand_acronym("AI")
-print(ai)  # Output: "AI Intelligent Assistant"
-
-# Example with a custom formula:
-expander = AIExpander(custom_acronym_formula)
-ai = expander.expand_acronym("ML")
-print(ai)  # Output: "Artificial ML Assistant"
-
-# Add your Discord bot token here
-bot.run('')
